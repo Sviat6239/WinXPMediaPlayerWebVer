3b6905d39576f451a67405321de071678fd2b993
Sviat6239 <sviatoslav6239@gmail.com>
2026-03-08 14:07:08 +0100

    declarated buttons in controlflow.js

---
+++ static/js/contlolFlow.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const backwardBtn = document.querySelector('.backward_btn');
+    const pauseBtn = document.querySelector('.pause_btn');
+    const playBtn = document.querySelector('.play_btn');
+    const forwardBtn = document.querySelector('.forward_btn');
+    const shuflBtn = document.querySelector('.sufl_btn');
+    const loopBtn = document.querySelector('.loop_btn');
+
+
+})
